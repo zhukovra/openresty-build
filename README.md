98cf2440d1a16d7cd7328dde1324116c76c4f2c8
Zhukov Roman <zhukov.roman@gmail.com>
2020-03-26 22:46:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Bash script for building Openresty from source.
+
+Origin script for Nginx is MatthewVance/nginx-build.
+
+### TESTING
+
+Tested on Debian 10.
+
+Test build script via docker:
+
+```bash
+# Test build
+docker run --rm -i debian:10 bash < build.sh
+```
+
+Test build script and service via Vagrant:
+
+```bash
+vagrant up
+vagrant ssh
+sudo bash /vagrant/build.sh
+sudo service nginx start
+sudo nginx -V
+```
---
+++ build.sh
@@ -0,0 +1,217 @@
+#!/usr/bin/env bash
+# Run as root or with sudo
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root or with sudo."
+  exit 1
+fi
+
+# Make script exit if a simple command fails and
+# Make script print commands being executed
+set -e -x
+
+# Ensure curl is installed
+apt-get update && apt-get install curl -y
+
+# Set URLs to the source directories
+source_pcre=https://ftp.pcre.org/pub/pcre/
+source_zlib=https://zlib.net/
+source_openssl=https://www.openssl.org/source/
+source_nginx=https://openresty.org/download/
+
+version_libmaxminddb=1.3.2
+source_libmaxminddb="https://github.com/maxmind/libmaxminddb/releases/download/${version_libmaxminddb}/libmaxminddb-${version_libmaxminddb}.tar.gz"
+
+version_ngx_http_geoip2_module=3.2
+source_ngx_http_geoip2_module="https://github.com/leev/ngx_http_geoip2_module/archive/${version_ngx_http_geoip2_module}.tar.gz"
+
+# Look up latest versions of each package
+version_pcre=$(curl -sL ${source_pcre} | grep -Eo 'pcre\-[0-9.]+[0-9]' | sort -V | tail -n 1)
+version_zlib=$(curl -sL ${source_zlib} | grep -Eo 'zlib\-[0-9.]+[0-9]' | sort -V | tail -n 1)
+version_openssl=$(curl -sL ${source_openssl} | grep -Eo 'openssl\-[0-9.]+[a-z]?' | sort -V | tail -n 1)
+version_nginx=openresty-1.15.8.3
+
+# Set OpenPGP keys used to sign downloads
+opgp_pcre=45F68D54BBE23FB3039B46E59766E084FB0F43D8
+opgp_zlib=5ED46A6721D365587791E2AA783FCD8E58BCAFBA
+opgp_openssl=8657ABB260F056B1E5190839D9C4D26D0E604491
+opgp_nginx=B0F4253373F8F6F510D42178520A9993A1C052F8
+
+# Set where OpenSSL and NGINX will be built
+bpath=$(pwd)/build
+
+# Make a "today" variable for use in back-up filenames later
+today=$(date +"%Y-%m-%d")
+
+# Clean out any files from previous runs of this script
+rm -rf \
+  "$bpath" \
+  /etc/nginx-default
+mkdir "$bpath"
+
+# Ensure the required software to compile NGINX is installed
+apt-get -y install \
+  binutils \
+  build-essential \
+  curl \
+  dirmngr \
+  libssl-dev
+
+# Download the source files
+curl -L "${source_pcre}${version_pcre}.tar.gz" -o "${bpath}/pcre.tar.gz"
+curl -L "${source_zlib}${version_zlib}.tar.gz" -o "${bpath}/zlib.tar.gz"
+curl -L "${source_openssl}${version_openssl}.tar.gz" -o "${bpath}/openssl.tar.gz"
+curl -L "${source_nginx}${version_nginx}.tar.gz" -o "${bpath}/nginx.tar.gz"
+curl -L "${source_libmaxminddb}" -o "${bpath}/libmaxminddb.tar.gz"
+curl -L "${source_ngx_http_geoip2_module}" -o "${bpath}/ngx_http_geoip2_module.tar.gz"
+
+# Download the signature files
+curl -L "${source_pcre}${version_pcre}.tar.gz.sig" -o "${bpath}/pcre.tar.gz.sig"
+curl -L "${source_zlib}${version_zlib}.tar.gz.asc" -o "${bpath}/zlib.tar.gz.asc"
+curl -L "${source_openssl}${version_openssl}.tar.gz.asc" -o "${bpath}/openssl.tar.gz.asc"
+# curl -L "${source_nginx}${version_nginx}.tar.gz.asc" -o "${bpath}/nginx.tar.gz.asc"
+
+# Verify the integrity and authenticity of the source files through their OpenPGP signature
+cd "$bpath"
+GNUPGHOME="$(mktemp -d)"
+export GNUPGHOME
+( gpg --keyserver ipv4.pool.sks-keyservers.net --recv-keys "$opgp_pcre" "$opgp_zlib" "$opgp_openssl" "$opgp_nginx" \
+|| gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$opgp_pcre" "$opgp_zlib" "$opgp_openssl" "$opgp_nginx")
+gpg --batch --verify pcre.tar.gz.sig pcre.tar.gz
+gpg --batch --verify zlib.tar.gz.asc zlib.tar.gz
+gpg --batch --verify openssl.tar.gz.asc openssl.tar.gz
+# gpg --batch --verify nginx.tar.gz.asc nginx.tar.gz
+
+# Expand the source files
+cd "$bpath"
+for archive in ./*.tar.gz; do
+  tar xzf "$archive"
+done
+
+# Clean up source files
+rm -rf \
+  "$GNUPGHOME" \
+  "$bpath"/*.tar.*
+
+# Build libmaxminddb
+cd "$bpath/libmaxminddb-${version_libmaxminddb}"
+./configure
+make
+make check
+make install
+ldconfig
+
+# Rename the existing /etc/nginx directory so it's saved as a back-up
+if [ -d "/etc/nginx" ]; then
+  mv /etc/nginx "/etc/nginx-${today}"
+fi
+
+# Create NGINX cache directories if they do not already exist
+if [ ! -d "/var/cache/nginx/" ]; then
+  mkdir -p \
+    /var/cache/nginx/client_temp \
+    /var/cache/nginx/proxy_temp \
+    /var/cache/nginx/fastcgi_temp \
+    /var/cache/nginx/uwsgi_temp \
+    /var/cache/nginx/scgi_temp
+fi
+
+# Add NGINX group and user if they do not already exist
+id -g nginx &>/dev/null || addgroup --system nginx
+id -u nginx &>/dev/null || adduser --disabled-password --system --home /var/cache/nginx --shell /sbin/nologin --group nginx
+
+# Test to see if our version of gcc supports __SIZEOF_INT128__
+if gcc -dM -E - </dev/null | grep -q __SIZEOF_INT128__
+then
+  ecflag="enable-ec_nistp_64_gcc_128"
+else
+  ecflag=""
+fi
+
+# Build NGINX, with various modules included/excluded
+cd "$bpath/$version_nginx"
+./configure \
+  -j2 \
+  --prefix=/etc/nginx \
+  --with-cc-opt="-O3 -fPIE -fstack-protector-strong -Wformat -Werror=format-security" \
+  --with-ld-opt="-Wl,-Bsymbolic-functions -Wl,-z,relro" \
+  --with-pcre="$bpath/$version_pcre" \
+  --with-zlib="$bpath/$version_zlib" \
+  --with-openssl-opt="no-weak-ssl-ciphers no-ssl3 no-shared $ecflag -DOPENSSL_NO_HEARTBEATS -fstack-protector-strong" \
+  --with-openssl="$bpath/$version_openssl" \
+  --sbin-path=/usr/sbin/nginx \
+  --modules-path=/usr/lib/nginx/modules \
+  --conf-path=/etc/nginx/nginx.conf \
+  --error-log-path=/var/log/nginx/error.log \
+  --http-log-path=/var/log/nginx/access.log \
+  --pid-path=/var/run/nginx.pid \
+  --lock-path=/var/run/nginx.lock \
+  --http-client-body-temp-path=/var/cache/nginx/client_temp \
+  --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+  --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+  --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+  --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+  --user=nginx \
+  --group=nginx \
+  --with-ipv6 \
+  --with-file-aio \
+  --with-http_auth_request_module \
+  --with-http_gunzip_module \
+  --with-http_gzip_static_module \
+  --with-http_mp4_module \
+  --with-http_realip_module \
+  --with-http_secure_link_module \
+  --with-http_slice_module \
+  --with-http_ssl_module \
+  --with-http_stub_status_module \
+  --with-http_sub_module \
+  --with-http_v2_module \
+  --with-pcre-jit \
+  --with-stream \
+  --with-stream_ssl_module \
+  --with-threads \
+  --without-http_empty_gif_module \
+  --without-http_geo_module \
+  --without-http_split_clients_module \
+  --without-http_ssi_module \
+  --without-mail_imap_module \
+  --without-mail_pop3_module \
+  --without-mail_smtp_module \
+  --add-module="$bpath/ngx_http_geoip2_module-$version_ngx_http_geoip2_module"
+make
+make install
+make clean
+strip -s /usr/sbin/nginx*
+
+if [ -d "/etc/nginx-${today}" ]; then
+  # Rename the default /etc/nginx settings directory so it's accessible as a reference to the new NGINX defaults
+  mv /etc/nginx /etc/nginx-default
+
+  # Restore the previous version of /etc/nginx to /etc/nginx so the old settings are kept
+  mv "/etc/nginx-${today}" /etc/nginx
+fi
+
+# Create NGINX systemd service file if it does not already exist
+if [ ! -e "/lib/systemd/system/nginx.service" ]; then
+  # Control will enter here if the NGINX service doesn't exist.
+  file="/lib/systemd/system/nginx.service"
+
+  /bin/cat >$file <<'EOF'
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network.target remote-fs.target nss-lookup.target
+[Service]
+Type=forking
+PIDFile=/var/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
+
+echo "All done.";
+echo "Start with sudo systemctl start nginx"
+echo "or with sudo nginx"
